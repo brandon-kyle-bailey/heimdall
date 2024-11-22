c4ac11c6fd0a10abee91877b9f9777559558e059
brandon-kyle-bailey <brandonkylebailey@outlook.com>
2024-11-21 22:10:52 -0400

    add linux and windows interfaces

---
+++ src/main/java/heimdall/adapters/LinuxActivityAdapter.java
@@ -0,0 +1,93 @@
+package heimdall.adapters;
+
+import java.time.LocalDateTime;
+
+import org.json.JSONObject;
+
+import heimdall.common.enums.EDomainEvents;
+import heimdall.common.interfaces.IActivityTracker;
+import heimdall.ports.LoggerPort;
+
+public class LinuxActivityAdapter implements IActivityTracker {
+
+  private EventbusAdapter _eventbus;
+
+  public LinuxActivityAdapter(EventbusAdapter eventbus) {
+    this._eventbus = eventbus;
+  }
+
+  @Override
+  public boolean hasPermissions() {
+    return true;
+  }
+
+  @Override
+  public void requestPermissions() {
+  }
+
+  @Override
+  public boolean isIdle() {
+    return false;
+  }
+
+  @Override
+  public boolean isSuspended() {
+    return false;
+  }
+
+  @Override
+  public boolean isLidClosed() {
+    return false;
+  }
+
+  @Override
+  public JSONObject getActiveWindow() {
+    return new JSONObject();
+  }
+
+  @Override
+  public boolean currentActivityEqualsLastActivity(JSONObject currentActivity, JSONObject lastActivity) {
+    if (currentActivity == null || lastActivity == null) {
+      return currentActivity == lastActivity;
+    }
+
+    boolean namesMatch = currentActivity.optString("name").equals(lastActivity.optString("name"));
+    boolean titlesMatch = currentActivity.optString("title").equals(lastActivity.optString("title"));
+    boolean urlsMatch = currentActivity.optString("url").equals(lastActivity.optString("url"));
+
+    return namesMatch && titlesMatch && urlsMatch;
+  }
+
+  @Override
+  public void run() {
+    if (!hasPermissions()) {
+      requestPermissions();
+      return;
+    }
+    JSONObject lastActivity = null;
+    while (true) {
+      JSONObject currentActivity = this.getActiveWindow();
+      if (currentActivity != null && !currentActivityEqualsLastActivity(currentActivity, lastActivity)) {
+        LoggerPort.debug("Activity changed: %s -> %s".formatted(lastActivity, currentActivity));
+
+        this._eventbus.publish(EDomainEvents.CREATE_APP.toString(), currentActivity);
+
+        if (lastActivity != null) {
+          lastActivity.put("endTime", LocalDateTime.now().toString());
+          this._eventbus.publish(EDomainEvents.UPSERT_ACTIVITY.toString(), lastActivity);
+        }
+
+        currentActivity.put("startTime", LocalDateTime.now().toString());
+        this._eventbus.publish(EDomainEvents.UPSERT_ACTIVITY.toString(), currentActivity);
+
+        lastActivity = currentActivity;
+
+        try {
+          Thread.sleep(100);
+        } catch (InterruptedException e) {
+          LoggerPort.error("Sleep interrupted: %s".formatted(e.getMessage()));
+        }
+      }
+    }
+  }
+}
--- src/main/java/heimdall/adapters/factories/ActivityTrackerAdapterFactory.java
+++ src/main/java/heimdall/adapters/factories/ActivityTrackerAdapterFactory.java
@@ -3,17 +3,19 @@ package heimdall.adapters.factories;
 import heimdall.common.interfaces.IActivityTracker;
 import heimdall.ports.LoggerPort;
 import heimdall.adapters.MacActivityAdapter;
+import heimdall.adapters.WindowsActivityAdapter;
 import heimdall.adapters.EventbusAdapter;
+import heimdall.adapters.LinuxActivityAdapter;
 
 public class ActivityTrackerAdapterFactory {
   public static IActivityTracker getAdapter(EventbusAdapter eventbus) throws Exception {
     String osName = System.getProperty("os.name").toLowerCase();
     if (osName.contains("win")) {
-      return new MacActivityAdapter(eventbus);
+      return new WindowsActivityAdapter(eventbus);
     } else if (osName.contains("mac")) {
       return new MacActivityAdapter(eventbus);
     } else if (osName.contains("nix") || osName.contains("nux") || osName.contains("aix")) {
-      return new MacActivityAdapter(eventbus);
+      return new LinuxActivityAdapter(eventbus);
     } else {
       LoggerPort.error("Unkown operating system: %s".formatted(osName));
       throw new Exception("Unknown operating system");
