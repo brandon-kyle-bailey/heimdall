97ce66609e80ab77128ee4f0bea7784ba361882b
brandon-kyle-bailey <brandon.bailey@produce8.com>
2024-11-28 18:05:51 -0400

    clean

--- scripts/build/macos.sh
+++ scripts/build/macos.sh
@@ -2,5 +2,5 @@ jpackage --name Heimdall \
   --input target \
   --main-jar core-1.0-SNAPSHOT.jar \
   --main-class heimdall.App \
-  --mac-entitlements ./src/main/resources/entitlements.plist \
+  --mac-entitlements ./src/main/resources/macos/entitlements.plist \
   --type dmg
